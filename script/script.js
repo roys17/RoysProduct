35c3a52ae55d7618c46048bf21c0e888df4e9335
Fakhriddin <fakhriddintogaev29@gmail.com>
2021-12-10 22:27:50 +0500

    create script

---
+++ script/script.js
@@ -0,0 +1,51 @@
+let btnToggler = document.querySelector('.nav__toggler');
+let navMenu = document.querySelector('.header__list-wrapper');
+
+btnToggler.addEventListener('click', function() {
+    this.classList.toggle('close');
+    navMenu.classList.toggle('show');
+})
+
+
+// var swiper = new Swiper(".main__commnets", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 7500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   loop: true,
+// });
+
+
+var swiper = new Swiper(".main__commnets", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop:true,
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
